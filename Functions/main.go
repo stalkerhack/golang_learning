026783f45e1432741aba942354b302c0318d3942
stalkerhack <stalkerhack@mail.ru>
2018-12-05 22:09:28 +0300

    Update...

--- Functions/main.go
+++ Functions/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	first_array := []float64{98, 93, 77, 82, 83}
+	var i int
 
+	total := 0.0
+	for i = 0; i < len(first_array); i++ {
+		total = total + first_array[i]
+	}
+
+	total = total / float64(len(first_array))
+	fmt.Println(total)
 }
