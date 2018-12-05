dc0bd952cd147370e626541cfda44e09e462a011
stalkerhack <stalkerhack@mail.ru>
2018-12-05 22:38:45 +0300

    Update...

--- Functions/main.go
+++ Functions/main.go
@@ -2,19 +2,20 @@ package main
 
 import "fmt"
 
-func average() {
-	panic("Not Implemented")
+func average(avarage_array []float64) float64 {
+	var i int
+
+	total := 0.0
+	for i = 0; i < len(avarage_array); i++ {
+		total = total + avarage_array[i]
+	}
+
+	total = total / float64(len(avarage_array))
+	return total
 }
 
 func main() {
 	first_array := []float64{98, 93, 77, 82, 83}
-	var i int
 
-	total := 0.0
-	for i = 0; i < len(first_array); i++ {
-		total = total + first_array[i]
-	}
-
-	total = total / float64(len(first_array))
-	fmt.Println(total)
+	fmt.Println(average(first_array))
 }
