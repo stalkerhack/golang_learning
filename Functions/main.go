af16ff32fb03396025d47159f49a0aa92e109870
stalkerhack <stalkerhack@mail.ru>
2018-12-06 21:55:02 +0300

    Update func...

--- Functions/main.go
+++ Functions/main.go
@@ -2,20 +2,36 @@ package main
 
 import "fmt"
 
+// Объявление и описание функции avarage
 func average(avarage_array []float64) (result float64) {
 	var i int
-
+	// Сложение всех элементов массива
 	total := 0.0
 	for i = 0; i < len(avarage_array); i++ {
 		total = total + avarage_array[i]
 	}
-
+	// Расчет среднего арифметического и запись в переменную result
 	result = total / float64(len(avarage_array))
 	return
 }
 
-func main() {
-	first_array := []float64{98, 93, 77, 82, 83}
+// Объявление и описание функции factorial
+func factorial(x uint) uint {
+	if x == 0 {
+		return 1
+	}
 
-	fmt.Println(average(first_array))
+	return x * factorial(x-1)
+}
+
+func main() {
+	fmt.Println("Введите число для расчета факториала: ")
+	var input_value uint
+	fmt.Scanln(&input_value)
+	// Объявление массива, который будет передан функции avarage
+	first_array := []float64{98, 93, 77, 82, 83}
+	// Вызов функции avarage, считающей среднее арифметическое
+	fmt.Println("\n\nСреднее арифметическое = ", average(first_array))
+	// Вызов функции factorial, считающей факториал
+	fmt.Println("Факториал числа = ", factorial(input_value))
 }
