75226485e49c89516d2c8ee5ce7f5ed4bb21b997
stalkerhack <stalkerhack@mail.ru>
2018-12-10 23:02:31 +0300

    Upda....I'm sleeeeping....Zzzz

--- Pointers/main.go
+++ Pointers/main.go
@@ -2,15 +2,18 @@ package main
 
 import "fmt"
 
-func zero(xPtr *int) {
-	*xPtr = 0
-	fmt.Println("*xPtr = ", *xPtr)
-	fmt.Println("xPtr = ", xPtr)
-	fmt.Println("&xPtr = ", &xPtr)
+func swap_function() {
+
 }
+
 func main() {
-	x := 5
-	zero(&x)
-	fmt.Println("\nx = ", x)
-	fmt.Println("&x = ", &x)
+	var x int
+	var y int
+
+	x = 23
+	y = 32
+	fmt.Println("X = ", x, "\n", "Y = ", y)
+	fmt.Println("\n\nSwap the numbers...Magic...\n\n", "Result: \n")
+
+	fmt.Println("X = ", x, "\n", "Y = ", y)
 }
