fac3ac08fdbcab201db24e7b7d488c2366b28eda
stalkerhack <stalkerhack@mail.ru>
2018-12-10 22:50:51 +0300

    Update. Pointers.

--- Pointers/main.go
+++ Pointers/main.go
@@ -4,9 +4,13 @@ import "fmt"
 
 func zero(xPtr *int) {
 	*xPtr = 0
+	fmt.Println("*xPtr = ", *xPtr)
+	fmt.Println("xPtr = ", xPtr)
+	fmt.Println("&xPtr = ", &xPtr)
 }
 func main() {
 	x := 5
 	zero(&x)
-	fmt.Println(x) // x всё еще равен 5
+	fmt.Println("\nx = ", x)
+	fmt.Println("&x = ", &x)
 }
