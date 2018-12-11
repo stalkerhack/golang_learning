a381bf693fb4d667093ec66e122db8bc2c898f10
stalkerhack <stalkerhack@mail.ru>
2018-12-11 22:36:33 +0300

    Update. Begin structures...

--- Pointers, structures, interfaces/main.go
+++ Pointers, structures, interfaces/main.go
@@ -1,20 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func swap_function(swap_x *int, swap_y *int) {
 	*swap_x = 32
 	*swap_y = 23
 }
 
-func main() {
-	var x int
-	var y int
+func distance(x1, y1, x2, y2 float64) float64 {
+	var a float64
+	var b float64
 
-	x = 23
-	y = 32
+	a = x2 - x1
+	b = y2 - y1
+	return math.Sqrt(a*a + b*b)
+}
+
+func rectangleArea(x1, y1, x2, y2 float64) float64 {
+	l := distance(x1, y1, x1, y2)
+	w := distance(x1, y1, x2, y1)
+	return l * w
+}
+
+func circleArea(x, y, r float64) float64 {
+	return math.Pi * r * r
+}
+
+func main() {
+	var x, y int = 23, 32
+	var rx1, ry1 float64 = 0, 0
+	var rx2, ry2 float64 = 10, 10
+	var cx, cy, cr float64 = 0, 0, 5
+
 	fmt.Println("X = ", x, "\n", "Y = ", y)
 	fmt.Println("\nSwap the numbers...Magic...\n", "Result: \n")
 	swap_function(&x, &y)
 	fmt.Println("X = ", x, "\n", "Y = ", y)
+
+	fmt.Println("\n", rectangleArea(rx1, ry1, rx2, ry2))
+	fmt.Println(circleArea(cx, cy, cr))
 }
