1909c1c1025e6e9a36f9e673220d962736507178
stalkerhack <stalkerhack@mail.ru>
2018-12-11 07:40:07 +0300

    Pointers lesson

--- Pointers/main.go
+++ Pointers/main.go
@@ -2,8 +2,9 @@ package main
 
 import "fmt"
 
-func swap_function() {
-
+func swap_function(swap_x *int, swap_y *int) {
+	*swap_x = 32
+	*swap_y = 23
 }
 
 func main() {
@@ -13,7 +14,7 @@ func main() {
 	x = 23
 	y = 32
 	fmt.Println("X = ", x, "\n", "Y = ", y)
-	fmt.Println("\n\nSwap the numbers...Magic...\n\n", "Result: \n")
-
+	fmt.Println("\nSwap the numbers...Magic...\n", "Result: \n")
+	swap_function(&x, &y)
 	fmt.Println("X = ", x, "\n", "Y = ", y)
 }
