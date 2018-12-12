8e89ab00ccb6b4affe312e390d65a1d00ce90efc
stalkerhack <stalkerhack@mail.ru>
2018-12-12 22:58:17 +0300

    Update.

--- Multithreading/main.go
+++ Multithreading/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func first_gorutine(n int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(n, ":", i)
+	}
+}
+
 func main() {
-	fmt.Println(Hello, World!)
+	go first_gorutine(123)
+	var input byte
+	fmt.Scanln(&input)
 }
