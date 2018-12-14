3b525be5574b2d99a169bd5b88875e337ca75029
Браславский Валерий Вячеславович <BraslavskyVV@pirogov-center.loc>
2018-12-14 10:54:54 +0300

    Learning...

--- Multithreading/main.go
+++ Multithreading/main.go
@@ -2,27 +2,52 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
-func pinger(c chan string) {
+func first__function_gorutine(n int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(n, ":", i)
+	}
+}
+
+func second_function_gorutine(n int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(n, ":", i)
+		amt := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * amt)
+	}
+}
+
+func message(c chan string) {
 	for i := 0; ; i++ {
 		c <- "Message..."
 	}
 }
-func printer(c chan string) {
+
+func message_output(c chan string) {
 	for {
 		msg := <-c
 		fmt.Println(msg)
 		time.Sleep(time.Second * 1)
 	}
 }
+
 func main() {
 	var c chan string = make(chan string)
 
-	go pinger(c)
-	go printer(c)
+	go first__function_gorutine(123)
+	fmt.Println("\n")
+	//////////////////////////////////////////
+	for i := 0; i < 10; i++ {
+		go second_function_gorutine(i)
+	}
+	//////////////////////////////////////////
+	go message(c)
+	go message_output(c)
 
 	var input string
 	fmt.Scanln(&input)
+
 }
