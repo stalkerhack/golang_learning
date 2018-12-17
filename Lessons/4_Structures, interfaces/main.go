b3330bf835f33e9d74310cdfb0197f81dfb351d2
stalkerhack <stalkerhack@mail.ru>
2018-12-17 20:10:49 +0300

    Restruct the projects

--- Lessons/4_Structures, interfaces/main.go
+++ Lessons/4_Structures, interfaces/main.go
@@ -5,11 +5,6 @@ import (
 	"math"
 )
 
-func swap_function(swap_x *int, swap_y *int) {
-	*swap_x = 32
-	*swap_y = 23
-}
-
 func distance(x1, y1, x2, y2 float64) float64 {
 	var a float64
 	var b float64
@@ -40,16 +35,10 @@ type Circle struct {
 }
 
 func main() {
-	var x, y int = 23, 32
 	var rx1, ry1 float64 = 0, 0
 	var rx2, ry2 float64 = 10, 10
 	//var cx, cy, cr float64 = 0, 0, 5
 	var c Circle
-
-	fmt.Println("X = ", x, "\n", "Y = ", y)
-	fmt.Println("\nSwap the numbers...Magic...\n", "Result: \n")
-	swap_function(&x, &y)
-	fmt.Println("X = ", x, "\n", "Y = ", y)
 
 	fmt.Println("\n", rectangleArea(rx1, ry1, rx2, ry2))
 	//fmt.Println(circleArea(cx, cy, cr))
