c72ee2e5a862c5844b281aace1050f5ed392eaa7
stalkerhack <stalkerhack@mail.ru>
2018-12-20 22:46:24 +0300

    Update

--- Test projects/GoSQL_bookshop/main.go
+++ Test projects/GoSQL_bookshop/main.go
@@ -1,17 +1,44 @@
 package main
 
-import {"fmt"
-		"database/sql"
-		_ "github.com/lib/pq"
-}
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
 
 type books struct {
-	isbn string
-	title string
+	isbn   string
+	title  string
 	author string
-	price float32	
+	price  float32
 }
 
 func main() {
-		
+	db, err := sql.Open("postgres", "postgres://admin:123@localhost")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err := db.Query("SELECT * FROM books")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	bks := make([]*Book, 0)
+	for rows.Next() {
+		bk := new(Book)
+		err := rows.Scan(&bk.isbn, &bk.title, &bk.author, &bk.price)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bks = append(bks, bk)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, bk := range bks {
+		fmt.Printf("%s, %s, %s, £%.2f\n", bk.isbn, bk.title, bk.author, bk.price)
+	}
 }
