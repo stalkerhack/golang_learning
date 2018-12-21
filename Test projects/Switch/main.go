0650a7d160607ce39c0bf999d89bb34ab504a7fc
stalkerhack <stalkerhack@mail.ru>
2018-12-21 17:39:24 +0300

    Switch_Case

--- Test projects/Switch/main.go
+++ Test projects/Switch/main.go
@@ -6,6 +6,16 @@ import (
 	"time"
 )
 
+func defer_func() {
+	fmt.Println("\ncounting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
+}
+
 func main() {
 	var today time.Weekday
 
@@ -22,4 +32,6 @@ func main() {
 		fmt.Println("Too far away.\n")
 	}
 	fmt.Println(reflect.TypeOf(today))
+
+	defer_func()
 }
